36fb40522f6cf9d877c7ad5b289e80a3c80dc775
ahmedashar <ahmedashar12345@gmail.com>
2022-05-24 19:59:24 +0500

    first commit

---
+++ 01_basics.js
@@ -0,0 +1,44 @@
+//01 alert, An alert is a box that pops up to give the user a message. Here's code for an alert that
+// displays the message "Thanks for your input!"
+
+// alert("Thanks for your input!");
+
+/////////////// 02 variable
+// variable are container for storing data/values, it can be change, 
+// two parts in var , decalaration + assignment
+// keyword varName = varValue; //initialization
+
+// code
+
+// var num1 = 10; 
+// var num2 = 20;
+// var total = num1 + num2;
+// console.log(total);
+
+// var userName; // declare
+// userName = "Ashar"; // assign
+// console.log(userName);
+
+//////////////03,  Prefix, Postfix, increment/Decrement
+
+// Postfix , first evaluate then increment
+// var x = 5;
+// var y = x++;
+// console.log(y); //5
+// console.log(x); //6
+
+// Prefix , first increment then evaluate
+// var x = 5;
+// var y = ++x;
+// console.log(y); //6
+// console.log(x); //6
+ 
+// Task --> do yourself without compiler (clear the concept of prefix and postfix)
+// var n = 5;
+// console.log(n+ n-- - ++n + --n * ++n - n); 
+//          5 + 5 -  5 +    4  *  5 - 5
+//             10  - 5 +    20  -5
+//               10    + 15    -5
+//                  25-5
+//                   20  
+
